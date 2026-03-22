9e244ad88b64801d8cb7dc0e57fb46d9401b706c
sebastian <seba1junio@gmail.com>
2026-03-22 14:21:32 -0300

    add app proveedores

---
+++ proveedores/urls.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+from .views import ProveedorViewSet
+
+router = DefaultRouter()
+router.register(r'proveedores', ProveedorViewSet, basename='proveedor')
+
+urlpatterns = router.urls
---
+++ proveedores/views.py
@@ -0,0 +1,26 @@
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .models import Proveedor
+from .serializers import ProveedorSerializer
+
+
+class ProveedorViewSet(viewsets.ModelViewSet):
+    queryset = Proveedor.objects.all()
+    serializer_class = ProveedorSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = [
+        "codigo",
+        "nombre",
+        "documento",
+        "email",
+        "telefono",
+        "ciudad",
+    ]
+    ordering_fields = [
+        "id",
+        "codigo",
+        "nombre",
+        "ciudad",
+        "creado_en",
+    ]
+    ordering = ["nombre"]
